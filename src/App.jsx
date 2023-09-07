566adb8a25d98011fb3f6fe9f3c169fb8228b210
mero2online <mero2online@gmail.com>
2023-09-08 00:00:32 +0300

    EDIT: Remove unused component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { loadData } from './Data';
+import { loadData, range } from './Data';
 import { PageSwiper } from './PageSwiper';
 import DrawerCustomized from './DrawerCustomized';
 const data = loadData();
 function App() {
   const [count, setCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [imgSrc, setImgSrc] = useState();
   const [subOptions, setSubOptions] = useState([1]);
 
   useEffect(() => {
-    // getImg(1);
     setCount(1);
   }, []);
-
-  const getImg = async (id) => {
-    setLoading(true);
-    const pageNo = String(id).padStart(3, '0');
-    const img = await fetch(
-      // `https://archive.org/download/mtmha_pages_AutoCorrected/page${pageNo}.png`
-      `./imgs/jpg/page${pageNo}.jpg`
-    );
-    setImgSrc(img.url);
-    setLoading(false);
-  };
-
-  const range = (start, end) =>
-    Array(end - start + 1)
-      .fill()
-      .map((_, idx) => start + idx);
 
   const handelOnSelectionChange = (idx) => {
     const sura = data[idx];
@@ -39,11 +20,9 @@ function App() {
         : range(sura.START_PAGE, sura.END_PAGE);
     setSubOptions(pagesPerSura);
     setCount(sura.START_PAGE);
-    // getImg(sura.START_PAGE);
   };
   const handelOnSelectionChangeSub = (page) => {
     setCount(Number(page));
-    // getImg(Number(page));
   };
 
   const renderOptions = (options) => {
@@ -59,6 +38,7 @@ function App() {
       </select>
     );
   };
+
   const renderSubOptions = (options) => {
     if (options)
       return (
@@ -73,15 +53,6 @@ function App() {
         </select>
       );
   };
-  const renderImage = () => {
-    if (!imgSrc) return <div></div>;
-    if (loading) return <div>Loading</div>;
-    return (
-      <div>
-        <img src={imgSrc} alt='img' className='img' />
-      </div>
-    );
-  };
 
   return (
     <>
@@ -94,49 +65,7 @@ function App() {
       />
       {renderOptions(data)}
       {renderSubOptions(subOptions)}
-      <div>
-        <button
-          className='MyBtn'
-          onClick={() => {
-            setCount(count + 1);
-            // getImg(count + 1);
-          }}
-          disabled={count >= 604 ? true : false}
-        >
-          Next
-        </button>
-        <span>{count}</span>
-        <button
-          className='MyBtn'
-          onClick={() => {
-            setCount(count - 1);
-            // getImg(count - 1);
-          }}
-          disabled={count <= 1 ? true : false}
-        >
-          Prev
-        </button>
-        <input
-          className='MyBtn'
-          type='number'
-          onChange={(e) => {
-            setImgSrc(null);
-            setCount(Number(e.target.value));
-          }}
-          min={1}
-          max={604}
-        />
-        <button
-          className='MyBtn'
-          onClick={() => {
-            // getImg(count);
-          }}
-          disabled={count <= 1 || count >= 605 ? true : false}
-        >
-          Go
-        </button>
-        {/* {renderImage()} */}
-      </div>
+      <div>{count}</div>
       <PageSwiper pageIdx={count} ranges={subOptions}></PageSwiper>
     </>
   );
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,3 +1,8 @@
 import jsonData from './QuranData.json';
 
 export const loadData = () => JSON.parse(JSON.stringify(jsonData));
+
+export const range = (start, end) =>
+  Array(end - start + 1)
+    .fill()
+    .map((_, idx) => start + idx);
