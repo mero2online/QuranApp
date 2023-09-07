8b9cd7a81d9acbb219a45ab0f4d9baf04d0869d1
mero2online <mero2online@gmail.com>
2023-09-08 01:29:00 +0300

    EDIT: Refactor app with QuranGUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { loadData, range } from './Data';
-import { PageSwiper } from './PageSwiper';
-import DrawerCustomized from './DrawerCustomized';
-const data = loadData();
+import QuranGUI from './QuranGUI';
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [subOptions, setSubOptions] = useState([1]);
-
-  useEffect(() => {
-    setCount(1);
-  }, []);
-
-  const handelOnSelectionChange = (idx) => {
-    const sura = data[idx];
-    const pagesPerSura =
-      sura.START_PAGE === sura.END_PAGE
-        ? [sura.START_PAGE]
-        : range(sura.START_PAGE, sura.END_PAGE);
-    setSubOptions(pagesPerSura);
-    setCount(sura.START_PAGE);
-  };
-  const handelOnSelectionChangeSub = (page) => {
-    setCount(Number(page));
-  };
-
   return (
     <>
       <div>Quran App</div>
-      <DrawerCustomized
-        Data={data}
-        handelOnSelectionChange={handelOnSelectionChange}
-        handelOnSelectionChangeSub={handelOnSelectionChangeSub}
-        pagesPerSura={subOptions}
-      />
-      <div>{count}</div>
-      <PageSwiper pageIdx={count} ranges={subOptions}></PageSwiper>
+      <QuranGUI />
     </>
   );
 }
---
+++ src/QuranGUI.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { loadData, range } from './Data';
+import { PageSwiper } from './PageSwiper';
+import DrawerCustomized from './DrawerCustomized';
+const data = loadData();
+
+const QuranGUI = () => {
+  const [count, setCount] = useState(0);
+  const [subOptions, setSubOptions] = useState([1]);
+
+  useEffect(() => {
+    setCount(1);
+  }, []);
+
+  const handelOnSelectionChange = (idx) => {
+    const sura = data[idx];
+    const pagesPerSura =
+      sura.START_PAGE === sura.END_PAGE
+        ? [sura.START_PAGE]
+        : range(sura.START_PAGE, sura.END_PAGE);
+    setSubOptions(pagesPerSura);
+    setCount(sura.START_PAGE);
+  };
+  const handelOnSelectionChangeSub = (page) => {
+    setCount(Number(page));
+  };
+
+  return (
+    <>
+      <DrawerCustomized
+        Data={data}
+        handelOnSelectionChange={handelOnSelectionChange}
+        handelOnSelectionChangeSub={handelOnSelectionChangeSub}
+        pagesPerSura={subOptions}
+      />
+      <div>{count}</div>
+      <PageSwiper pageIdx={count} ranges={subOptions}></PageSwiper>
+    </>
+  );
+};
+
+export default QuranGUI;
